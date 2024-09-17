f7f997ee029c464a6309cff8efda616b6f665d0d
Blake Rossi <bcr4475@rit.edu>
2024-09-17 12:05:02 -0400

    Basic Movement

---
+++ Indiecisive-Unity/Assets/Scripts/Planet/InputController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public float moveSpeed = 5f;  // Speed at which the player moves
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Get input from WASD keys or arrow keys
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Apply movement based on input and moveSpeed
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
