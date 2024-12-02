3eb11ca59172fd2b951af842d33f122371611d43
Nicholas Amico <nva4602@rit.edu>
2024-12-02 10:17:31 -0500

    yooooo

--- Indiecisive-Unity/Assets/Art/Sound/SoundService.cs
+++ Indiecisive-Unity/Assets/Art/Sound/SoundService.cs
@@ -1,34 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundService : MonoBehaviour
 {
-    [SerializeField] AudioSource source;
+
+    // # of AudioSources you put inside the Empty GameObject (in this case, 10)
+    [SerializeField] AudioSource source0;
+    [SerializeField] AudioSource source1;
+    [SerializeField] AudioSource source2;
+    [SerializeField] AudioSource source3;
+    [SerializeField] AudioSource source4;
+    [SerializeField] AudioSource source5;
+    [SerializeField] AudioSource source6;
+    [SerializeField] AudioSource source7;
+    [SerializeField] AudioSource source8;
+    [SerializeField] AudioSource source9;
+
+
+    AudioSource[] AudioSourceArray = new AudioSource[10];
+
+    // Add add one for each sound you will need for this script's usage (in this case, 2)
     [SerializeField] AudioClip clip1;
+    [SerializeField] AudioClip clip2;
 
     //private bool isPlayingFootsteps = false;
 
     // Update is called once per frame
     void Start()
     {
-        //source = GetComponent<AudioSource>();
-        //source.clip = clip;
+        AudioSourceArray[0] = source0;
+        AudioSourceArray[1] = source1;
+        AudioSourceArray[2] = source2;
+        AudioSourceArray[3] = source3;
+        AudioSourceArray[4] = source4;
+        AudioSourceArray[5] = source5;
+        AudioSourceArray[6] = source6;
+        AudioSourceArray[7] = source7;
+        AudioSourceArray[8] = source8;
+        AudioSourceArray[9] = source9;
     }
     void Update()
     {
-        /*
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
-        {
-            // Debug.Log("space key was pressed");
-            //source.Play();
-            isPlayingFootsteps = true;
-        }*
-        */
         if (Input.GetKeyDown(KeyCode.P))
         {
-            source.clip = clip1;
-            source.Play();
+            playSound(clip1);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            playSound(clip2);
+        }
+
+    }
+    void playSound(AudioClip clip)
+    {
+        for (int i = 0; i < AudioSourceArray.Length; ++i)
+        {
+            if (AudioSourceArray[i].isPlaying == false)
+            {
+                AudioSourceArray[i].clip = clip;
+                AudioSourceArray[i].Play();
+                break;
+            }
         }
     }
 }
